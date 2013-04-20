f8f80e5bfae8a4e5059f5eddefcee33537696ddb
Assi <Assi.Ivomir@gmail.com>
2013-04-20 18:47:49 +0300

    Adding Homework 7. Methods

---
+++ Methods.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Methods
+{
+    class Methods
+    {
+        //1. Promenih da hvarlq exception vmesto cw
+        static double CalcTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Sides should be positive.");
+                //Console.Error.WriteLine("Sides should be positive.");
+                //return -1;
+            }
+
+            double heronFormula = (a + b + c) / 2;
+            double area = Math.Sqrt(
+                (heronFormula *
+                (heronFormula - a) *
+                (heronFormula - b) *
+                (heronFormula - c)));
+
+            return area;
+        }
+
+        // 1. smqna na imeto na metoda
+        // 2. hvarlqne na exception
+        static string ConvertDigitToWord(int number)
+        {
+            switch (number)
+            {
+                case 0: return "zero";
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+            }
+
+            throw new ArgumentException("Invalid number!");
+        }
+
+        // 1. smqna na imeto na metoda
+        // 2. razdelqne na proverkata na dva otdelni ifa
+        // 3. opravqne na for-a da nqma strani4en efekt da promenq [0]
+        static int FindMaxNumber(params int[] elements)
+        {
+            if (elements == null)
+            {
+                throw new ArgumentNullException("Input can not be null!");
+            }
+
+            if (elements.Length == 0)
+            {
+                throw new ArgumentException("Array must have at least one element!");
+            }
+
+            int maxElement = int.MinValue;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i] > maxElement)
+                {
+                    maxElement = elements[i];
+                }
+            }
+
+            return maxElement;
+        }
+
+        // 1. razdelqme methoda na 3 novi
+
+        static void PrintNumberWithPrecionTwo(double number)
+        { 
+            Console.WriteLine("{0:f2}", number); 
+        }
+        static void PrintNumberMultiplaiedByHundredPercent(double number) 
+        {
+            Console.WriteLine("{0:p0}", number);
+        }
+        static void PrintNumberWithAlignmentEight(double number)
+        {
+            Console.WriteLine("{0,8}", number);
+        }
+
+        static double CalcDistanceBetweenTwoPoints(double x1, double y1, double x2, double y2,
+            out bool isHorizontal, out bool isVertical)
+        {
+            isHorizontal = (y1 == y2);
+            isVertical = (x1 == x2);
+
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            
+            return distance;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+
+            Console.WriteLine(ConvertDigitToWord(5));
+
+            Console.WriteLine(FindMaxNumber(5, -1, 3, 2, 14, 2, 3));
+
+            PrintNumberWithPrecionTwo(1.3);
+            PrintNumberMultiplaiedByHundredPercent(0.75);
+            PrintNumberWithAlignmentEight(2.30);
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalcDistanceBetweenTwoPoints(3, -1, 3, 2.5, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            Student peter = new Student() { FirstName = "Peter", LastName = "Ivanov" };
+            peter.OtherInfo = "From Sofia, born at 17.03.1992";
+
+            Student stella = new Student() { FirstName = "Stella", LastName = "Markova" };
+            stella.OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993";
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
